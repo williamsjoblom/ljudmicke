e15de4d1d2a8ca9331553914f3b3d78da055a7b7
William Sjöblom <wllmsjblm@gmail.com>
2021-01-27 17:20:57 +0100

    Update toolbar with prettier aesthetics and mockup MIDI-in selector

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -11,13 +11,16 @@ import { play } from '../audio';
 
 import * as Colors from '../colors';
 
+import Icon from '@mdi/react';
+import { mdiMidi, mdiMetronome, mdiMidiPort } from '@mdi/js';
+
 
 const style = {
     width: '100%',
     height: '40px',
     backgroundColor: Colors.bgLight,//'#545454',
     borderBottom: '1px solid #444444',
-    paddingTop: '8px',
+    padding: '5px 0',
     margin: '0',
     textAlign: 'left',
 };
@@ -31,11 +34,13 @@ class ToolBar extends React.Component {
         return <nav style={style}>
 
                  <h1 style={{ display: 'inline',
-                              color: Colors.fgSecondary,
-                              fontVariant: 'small-caps',
+                              color: Colors.fgTernary,
+                              fontSize: '27pt',
+                              fontWeight: '100',
                               margin: '0 12px'}}>
-                   Ljudmicke
+                   ljud<span style={{color: Colors.fgSecondary}}>micke</span>
                  </h1>
+
                  <FaButton icon={faPlay}
                            width={30}
                            height={30}
@@ -44,11 +49,18 @@ class ToolBar extends React.Component {
                            width={30}
                            height={30}
                            onClick={() => alert('not implemented')}/>
+
                  <LabeledInput value={this.props.beatsPerMinute}
+                               icon={mdiMetronome}
                                label={'bpm'}
                                width={30}
                                height={30}
                                onChanged={this.props.setBeatsPerMinute}/>
+
+                 <LabeledInput value={"M-Audio KeyStudio"}
+                               icon={mdiMidi}
+                               width={150}
+                               height={30} />
                </nav>;
     }
 }
