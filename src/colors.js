d7ff0ce249baff5a8bd654ae75e6b749ec1bb363
William Sjöblom <wllmsjblm@gmail.com>
2021-01-27 17:18:02 +0100

    Update color scheme

--- src/colors.js
+++ src/colors.js
@@ -1,23 +1,35 @@
 export const timelinePalette = [
-    '#9C0D0D',
-    '#9C540D',
-    '#9C9C0C',
-    '#0D9B32',
-    '#9B0D9C',
-    '#5E2CA3',
-    '#3245A6',
-    '#9C0D0D',
-    '#9C540D',
-    '#0D9B32',
-    '#9B0D9C',
+    // '#9C0D0D',
+    // '#9C540D',
+    // '#9C9C0C',
+    // '#0D9B32',
+    // '#9B0D9C',
+    // '#5E2CA3',
+    // '#3245A6',
+    // '#9C0D0D',
+    // '#9C540D',
+    // '#0D9B32',
+    // '#9B0D9C',
+    '#F94144',
+    '#F3722C',
+    '#F8961E',
+    '#F9844A',
+    '#F9C74F',
+    '#90BE6D',
+    '#43AA8B',
+    '#4D908E',
+    '#577590',
+    '#277DA1',
 ];
 
 export const fgPrimary = '#F0F0F0';
 export const fgSecondary = '#C6C6C6';
+export const fgTernary = '#9C9C9C';
+
 export const bgLighter = '#505050';
 export const bgLight = '#454545';
 export const bgDark = '#333333';
 export const bgDarker = '#2D2D2D';
 export const bgDarkest = '#282828';
 
-export const bgTrackDivider = bgDarkest;
+export const bgTrackDivider = bgLight;
