44ae6805a31fa770c5b573320001a058acf7b9b6
William Sjöblom <wllmsjblm@gmail.com>
2021-02-07 16:00:13 +0100

    Add ParamFader

---
+++ src/components/ParamFader.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import Fader from './Fader';
+
+class ParamFader extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onChange = this.onChange.bind(this);
+        this.setValue = this.setValue.bind(this);
+
+        this.setValue(this.props.value);
+    }
+
+    setValue(value) {
+        console.log('Setting fader param');
+        if (Array.isArray(this.props.param)) {
+            this.props.param.forEach(
+                param => param.value = value
+            );
+        } else {
+            this.props.param.value = value;
+        }
+    }
+
+    onChange(value) {
+        this.props.onChange(value);
+        this.setValue(value);
+    }
+
+    render() {
+        return <Fader {...this.props}
+                      onChange={this.onChange}/>;
+    }
+}
+
+export default ParamFader;
